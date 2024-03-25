e5ee50dcb4b4b7bf6eee5a8933f92204c5371fa2
lightrao <lightrao@hotmail.com>
2024-03-25 08:05:00 +0800

    improve BasicNft.sol

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract basicNft is ERC721 {
-    uint256 private s_tokenCounter; // token ID
+contract BasicNft is ERC721 {
+    error BasicNft__TokenUriNotFound();
+
     mapping(uint256 => string) private s_tokenIdToUri;
+    uint256 private s_tokenCounter; // token ID
 
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
@@ -20,6 +22,13 @@ contract basicNft is ERC721 {
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert BasicNft__TokenUriNotFound();
+        }
         return s_tokenIdToUri[tokenId];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
